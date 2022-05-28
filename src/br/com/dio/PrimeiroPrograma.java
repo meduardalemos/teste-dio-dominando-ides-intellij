59f1b889b60199098712e1dca8bf5c82a26c1976
meduardalemos <maria.eduarda.lemos@hotmail.com>
2022-05-28 20:17:13 -0300

    commit teste através do IntelliJ

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,12 +4,12 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        /*int a = 5;
+        int a = 5;
         int b = 3;
-        System.out.println("Hello World! " + (a+b));*/
+        System.out.println("Hello World! " + (a+b));
 
-        Gato gato = new Gato();
+        /*Gato gato = new Gato();
 
-        System.out.println(gato);
+        System.out.println(gato);*/
     }
 }
